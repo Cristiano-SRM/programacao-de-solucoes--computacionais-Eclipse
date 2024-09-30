ce2577d6e94578c822084167a149b84891c6f00b
Cristiano <vitor@LAPTOP-8AAA07GF>
2024-09-30 19:05:47 -0300

    Atividade 4 condicional

---
+++ javaEstruturaSequencial/src/javaEstruturaCondicional/EstruturaSequencial4PANEL.java
@@ -0,0 +1,27 @@
+package javaEstruturaCondicional;
+import javax.swing.JOptionPane;
+
+public class EstruturaSequencial4PANEL {
+
+	public static void main(String[] args) {
+     String peso, altura;
+     peso=JOptionPane.showInputDialog("Informe seu peso: ");
+     altura=JOptionPane.showInputDialog("Informe sua altura: ");
+     double Peso=Double.parseDouble(peso);
+     double Altura=Double.parseDouble(altura);
+     
+     double imc=Peso/(Math.pow(Altura, 2));
+     if (imc<20) {
+   	 JOptionPane.showMessageDialog(null,"Abaixo do peso" );
+     }
+     if (imc>=20 && imc<=25) {
+     JOptionPane.showMessageDialog(null,"Normal");
+     }
+     if (imc>=25 && imc<=30) {
+     JOptionPane.showMessageDialog(null,"Sobrepeso");
+     }
+     if (imc>=30 && imc<=40) {
+     JOptionPane.showMessageDialog(null,"Obesidade");
+	}
+  }
+}
